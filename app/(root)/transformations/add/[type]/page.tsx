1a189ff0ae5a5007791a57bf0af21b26351332b8
imguna2218 <gunasekharparisa2218@gmail.com>
2024-12-25 09:06:28 +0530

    Initial commit

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>
+      <h1>Add Transformation Type</h1>
+    </div>
+  )
+}
+
+export default AddTransformationTypePage
